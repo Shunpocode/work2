2bbd43aefff1fe91198dc625a75c3cc4c1843fd1
Maksym <139382157+Shunpocode@users.noreply.github.com>
2023-12-22 05:10:51 +0000

    new html adn css

---
+++ _tmp_/js/js.js
@@ -0,0 +1,107 @@
+var section = document.getElementsByTagName('section');
+var navA = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
+var coverNav = document.getElementById('cover-nav');
+setInterval(() => {
+    if (document.getElementsByTagName('article')[0].getBoundingClientRect().y < 80) {
+        document.getElementsByTagName("header")[0].className = 'headerBlur';
+    }else{document.getElementsByTagName("header")[0].className = '';}
+    }, 10);
+
+ setInterval(() => {
+
+    if (section[1].getBoundingClientRect().y < 180 && section[1].getBoundingClientRect().y >= -section[1].offsetHeight + 180) {
+        coverNav.style.width = navA[0].offsetWidth + 'px';
+        coverNav.style.left = navA[0].offsetLeft + 'px';
+        navA[0].style.color = 'white';
+        coverNav.style.opacity = '1';
+    }else if(section[1].getBoundingClientRect().y > 0){
+        navA[0].style.color = '';
+        coverNav.style.opacity = '0';
+    }else{coverNav.style.opacity = '1'; navA[0].style.color = '';}
+
+    if (section[2].getBoundingClientRect().y < 180 && section[2].getBoundingClientRect().y >= -section[2].offsetHeight + 180) {
+        coverNav.style.width = navA[1].offsetWidth + 'px';
+        coverNav.style.left = navA[1].offsetLeft + 'px';
+        navA[1].style.color = 'white';
+    }else{navA[1].style.color = '';}
+
+    if (section[3].getBoundingClientRect().y < 180 && section[3].getBoundingClientRect().y >= -section[3].offsetHeight + 40) {
+        coverNav.style.width = navA[2].offsetWidth + 'px';
+        coverNav.style.left = navA[2].offsetLeft + 'px';
+        if(section[3].getBoundingClientRect().y >= -section[3].offsetHeight + 181){navA[2].style.color = 'white';}else{navA[2].style.color = 'black';}
+        coverNav.style.background = 'white';
+        document.getElementById('header').color = 'white !important';
+        document.getElementsByTagName('header')[0].style.background = 'rgba(0,0,0,.5)';
+        document.getElementsByTagName('nav')[0].className = 'blackBack';
+        document.getElementById('header').getElementsByTagName('button')[0].style.filter = 'invert(1)';
+        document.getElementsByClassName('logo')[0].style.color = 'white';
+    }
+    else{
+        document.getElementsByTagName('header')[0].style.background = '';
+        navA[2].style.color = '';
+        document.getElementsByTagName('nav')[0].className = '';
+        coverNav.style.background = '';
+        document.getElementById('header').getElementsByTagName('button')[0].style.filter = '';
+        document.getElementsByClassName('logo')[0].style.color = '';
+
+    }
+
+    if (section[4].getBoundingClientRect().y < 180 && section[4].getBoundingClientRect().y >= -section[4].offsetHeight) {
+        coverNav.style.width = navA[3].offsetWidth + 'px';
+        coverNav.style.left = navA[3].offsetLeft + 'px';
+        navA[3].style.color = 'white';
+    }else{navA[3].style.color = '';}
+}, 100);
+
+
+function scrollSection(p) {
+
+    window.scroll({
+
+        top: section[p].offsetTop + 1,
+        behavior: "smooth",
+
+    })
+
+}
+
+function dropDown(target) {
+    if (target == '0') {
+        var menu = document.getElementsByClassName('menu')[target];
+        if(menu.className == 'menu'){
+            document.getElementById('dropSvg').style.transform = 'rotate(0) translateY(-5px)';
+            menu.className = 'menu dropMenus';
+            
+        }else{
+            menu.className = 'menu'
+            document.getElementById('dropSvg').style.transform = '';
+        }
+
+    }
+
+}
+
+function telValue() {
+    var tel = document.getElementById('tel');
+    setInterval(() => {
+        if (tel.value.length <= 4) {
+            tel.value = '+380-';
+        }
+    }, 10);
+    tel.onkeypress = event => {
+        event.preventDefault();
+        if (tel.value.length <= 15) {
+            if (event.key.length > 1) return true;
+                tel.value = (tel.value+event.key).replace( /[^\d-]/g, '');
+            if (tel.value.length == 6) {
+                tel.value = (tel.value + '-');
+            }
+            if(tel.value.length == 10) {
+                tel.value = (tel.value + '-');
+            }
+            if (tel.value[0] !== '+') {
+                tel.value = '+' + tel.value;
+            }
+        }
+    }
+}
