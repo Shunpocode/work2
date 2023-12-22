361c0b91a37a71757aa845fb859500e3fd2abddf
Maksym <139382157+Shunpocode@users.noreply.github.com>
2023-12-22 11:43:34 +0200

    js
    js

--- tmp/js/js.js
+++ tmp/js/js.js
@@ -2,66 +2,125 @@ var section = document.getElementsByTagName('section');
 var navA = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
 var coverNav = document.getElementById('cover-nav');
 setInterval(() => {
-    if (document.getElementsByTagName('article')[0].getBoundingClientRect().y < 80) {
+    if (document.getElementsByTagName('article')[0].getBoundingClientRect().y < 80 * 2) {
         document.getElementsByTagName("header")[0].className = 'headerBlur';
     }else{document.getElementsByTagName("header")[0].className = '';}
     }, 10);
 
  setInterval(() => {
 
-    if (section[1].getBoundingClientRect().y < 180 && section[1].getBoundingClientRect().y >= -section[1].offsetHeight + 180) {
-        coverNav.style.width = navA[0].offsetWidth + 'px';
-        coverNav.style.left = navA[0].offsetLeft + 'px';
-        navA[0].style.color = 'white';
-        coverNav.style.opacity = '1';
-    }else if(section[1].getBoundingClientRect().y > 0){
-        navA[0].style.color = '';
-        coverNav.style.opacity = '0';
-    }else{coverNav.style.opacity = '1'; navA[0].style.color = '';}
-
-    if (section[2].getBoundingClientRect().y < 180 && section[2].getBoundingClientRect().y >= -section[2].offsetHeight + 180) {
-        coverNav.style.width = navA[1].offsetWidth + 'px';
-        coverNav.style.left = navA[1].offsetLeft + 'px';
-        navA[1].style.color = 'white';
-    }else{navA[1].style.color = '';}
-
-    if (section[3].getBoundingClientRect().y < 180 && section[3].getBoundingClientRect().y >= -section[3].offsetHeight + 40) {
-        coverNav.style.width = navA[2].offsetWidth + 'px';
-        coverNav.style.left = navA[2].offsetLeft + 'px';
-        if(section[3].getBoundingClientRect().y >= -section[3].offsetHeight + 181){navA[2].style.color = 'white';}else{navA[2].style.color = 'black';}
-        coverNav.style.background = 'white';
-        document.getElementById('header').color = 'white !important';
-        document.getElementsByTagName('header')[0].style.background = 'rgba(0,0,0,.5)';
-        document.getElementsByTagName('nav')[0].className = 'blackBack';
-        document.getElementById('header').getElementsByTagName('button')[0].style.filter = 'invert(1)';
-        document.getElementsByClassName('logo')[0].style.color = 'white';
+    if (document.body.clientWidth >= 1201) {
+        if (section[1].getBoundingClientRect().y < 180 && section[1].getBoundingClientRect().y >= -section[1].offsetHeight + 180) {
+            coverNav.style.width = navA[0].offsetWidth + 'px';
+            coverNav.style.left = navA[0].offsetLeft + 'px';
+            navA[0].style.color = 'white';
+            coverNav.style.opacity = '1';
+        }else if(section[1].getBoundingClientRect().y > 0){
+            navA[0].style.color = '';
+            coverNav.style.opacity = '0';
+        }else{coverNav.style.opacity = '1'; navA[0].style.color = '';}
+    
+        if (section[2].getBoundingClientRect().y < 180 && section[2].getBoundingClientRect().y >= -section[2].offsetHeight + 180) {
+            coverNav.style.width = navA[1].offsetWidth + 'px';
+            coverNav.style.left = navA[1].offsetLeft + 'px';
+            navA[1].style.color = 'white';
+        }else{navA[1].style.color = '';}
+    
+        if (section[3].getBoundingClientRect().y < 180 && section[3].getBoundingClientRect().y >= -section[3].offsetHeight + 10) {
+            coverNav.style.width = navA[2].offsetWidth + 'px';
+            coverNav.style.left = navA[2].offsetLeft + 'px';
+            if(section[3].getBoundingClientRect().y < -section[3].offsetHeight + 180){navA[2].style.color = 'black';}else{navA[2].style.color = 'white';}
+            coverNav.style.background = 'white';
+            document.getElementById('header').color = 'white !important';
+            document.getElementsByTagName('header')[0].style.background = 'rgba(0,0,0,.5)';
+            document.getElementsByTagName('nav')[0].className = 'blackBack';
+            document.getElementById('header').getElementsByTagName('button')[0].style.filter = 'invert(1)';
+            document.getElementsByClassName('logo')[0].style.color = 'white';
+        }
+        else{
+            document.getElementsByTagName('header')[0].style.background = '';
+            navA[2].style.color = '';
+            document.getElementsByTagName('nav')[0].className = '';
+            coverNav.style.background = '';
+            document.getElementById('header').getElementsByTagName('button')[0].style.filter = '';
+            document.getElementsByClassName('logo')[0].style.color = '';
+    
+        }
+    
+        if (section[4].getBoundingClientRect().y < 180 && section[4].getBoundingClientRect().y >= -section[4].offsetHeight) {
+            coverNav.style.width = navA[3].offsetWidth + 'px';
+            coverNav.style.left = navA[3].offsetLeft + 'px';
+            navA[3].style.color = 'white';
+        }else{navA[3].style.color = '';}
     }
-    else{
-        document.getElementsByTagName('header')[0].style.background = '';
-        navA[2].style.color = '';
-        document.getElementsByTagName('nav')[0].className = '';
-        coverNav.style.background = '';
-        document.getElementById('header').getElementsByTagName('button')[0].style.filter = '';
-        document.getElementsByClassName('logo')[0].style.color = '';
-
+    else if (document.body.clientWidth <= 1200) {
+        if (section[1].getBoundingClientRect().y < (180 * 2) && section[1].getBoundingClientRect().y >= -section[1].offsetHeight + 140) {
+            console.log(section[1].getBoundingClientRect().y,-section[1].offsetHeight + 160);
+            coverNav.style.width = navA[0].offsetWidth + 'px';
+            coverNav.style.left = navA[0].offsetLeft + 'px';
+            navA[0].style.color = 'white';
+            coverNav.style.opacity = '1';
+        }else if(section[1].getBoundingClientRect().y > 0){
+            navA[0].style.color = '';
+            coverNav.style.opacity = '0';
+        }else{coverNav.style.opacity = '1'; navA[0].style.color = '';}
+    
+        if (section[2].getBoundingClientRect().y < 130 && section[2].getBoundingClientRect().y >= -section[2].offsetHeight + -60) {
+            console.log(section[2].getBoundingClientRect().y,-section[2].offsetHeight + (180 * 2));
+            coverNav.style.width = navA[1].offsetWidth + 'px';
+            coverNav.style.left = navA[1].offsetLeft + 'px';
+            navA[1].style.color = 'white';
+        }else{navA[1].style.color = '';}
+    
+        if (section[3].getBoundingClientRect().y < 140 && section[3].getBoundingClientRect().y >= -section[3].offsetHeight + -15    0) {
+            coverNav.style.width = navA[2].offsetWidth + 'px';
+            coverNav.style.left = navA[2].offsetLeft + 'px';
+            if(section[3].getBoundingClientRect().y < -section[3].offsetHeight + 180){navA[2].style.color = 'black';}else{navA[2].style.color = 'white';}
+            coverNav.style.background = 'white';
+            document.getElementById('header').color = 'white !important';
+            document.getElementsByTagName('header')[0].style.background = 'rgba(0,0,0,.5)';
+            document.getElementsByTagName('nav')[0].className = 'blackBack';
+            document.getElementById('header').getElementsByTagName('button')[0].style.filter = 'invert(1)';
+            document.getElementsByClassName('logo')[0].style.color = 'white';
+        }
+        else{
+            document.getElementsByTagName('header')[0].style.background = '';
+            navA[2].style.color = '';
+            document.getElementsByTagName('nav')[0].className = '';
+            coverNav.style.background = '';
+            document.getElementById('header').getElementsByTagName('button')[0].style.filter = '';
+            document.getElementsByClassName('logo')[0].style.color = '';
+    
+        }
+    
+        if (section[4].getBoundingClientRect().y < (180 * 2) && section[4].getBoundingClientRect().y >= -section[4].offsetHeight) {
+            coverNav.style.width = navA[3].offsetWidth + 'px';
+            coverNav.style.left = navA[3].offsetLeft + 'px';
+            navA[3].style.color = 'white';
+        }else{navA[3].style.color = '';}
     }
-
-    if (section[4].getBoundingClientRect().y < 180 && section[4].getBoundingClientRect().y >= -section[4].offsetHeight) {
-        coverNav.style.width = navA[3].offsetWidth + 'px';
-        coverNav.style.left = navA[3].offsetLeft + 'px';
-        navA[3].style.color = 'white';
-    }else{navA[3].style.color = '';}
+    
 }, 100);
 
 
 function scrollSection(p) {
 
-    window.scroll({
+    if (window.pageXOffset > 1200) {
+        window.scroll({
 
-        top: section[p].offsetTop + 1,
-        behavior: "smooth",
+            top: section[p].offsetTop - 5,
+            behavior: "smooth",
+    
+        })
+    }
+    if (window.pageXOffset <= 1200) {
+        window.scroll({
 
-    })
+            top: section[p].offsetTop - 30,
+            behavior: "smooth",
+    
+        })
+    }
 
 }
 
